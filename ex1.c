5bd28643a7ffce24e9dcfca60c62531d6e64c8bf
Boring-1 <56294062+Boring-1@users.noreply.github.com>
2020-01-07 16:44:08 +0800

    Create ex1.c

---
+++ ex1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+
+{
+
+int a, b, sum;
+
+printf("please input two num!\n");
+
+scanf("%d,%d",&a,&b);
+
+sum = a+b;
+
+printf("sum is %d\n",sum);
+
+return 0;
+{
+    int result=sum*sum;
+    return result;
+}
+}
